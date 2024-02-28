0e4080feaee68f193287ac5fba1e5311cc586281
sindirahayu130605 <161553880+sindirahayu130605@users.noreply.github.com>
2024-02-28 11:41:41 +0700

    hapus_album.php

---
+++ hapus_album.php
@@ -0,0 +1,11 @@
+<?php
+include "koneksi.php";
+      session_start();
+
+
+      $albumid=$_GET['albumid'];
+
+      $sql=mysqli_query($conn, "delete from album where albumid='$albumid'");
+
+      header("location:album.php");
+    ?>
